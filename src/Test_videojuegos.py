2c9937e65557dcfd2a89b2f0a49c3ebd8ec2a24d
yisuscc <jescarcar5@alum.us.es>
2020-12-28 18:14:09 +0100

    hechas 2 funciones

--- src/Test_videojuegos.py
+++ src/Test_videojuegos.py
@@ -3,3 +3,8 @@ Created on 16 dic. 2020
 
 @author: jesus
 '''
+from videojuegos import *
+ruta = '../data/videojuegos.csv'
+lista_juegos = lee_juegos(ruta)
+#print(lista_juegos)
+print(genero_mas_presente(lista_juegos))
--- src/videojuegos.py
+++ src/videojuegos.py
@@ -5,7 +5,7 @@ Created on 16 dic. 2020
 '''
 import csv
 from matplotlib import pylab as plt
-from collections import namedtuple
+from collections import namedtuple, Counter
 
 Juego = namedtuple('Juego','rank, name, platform, year, genre, publisher, NA_sales, EU_sales, JP_sales, other_sales, global_sales')
 
@@ -14,7 +14,7 @@ def lee_juegos(fichero):
     with open(fichero,'r', encoding='utf-8')as f:
         lector = csv.reader(f)
         next(lector)
-        res = [Juego(int(rank), str(name),str(platform),int(year), str(genre),float(NA_sales),float(EU_sales),float(JP_sales),float(other_sales),float(global_sales))for rank, name, platform, year, genre, publisher, NA_sales, EU_sales, JP_sales, other_sales, global_sales in lector]
+        res = [Juego(int(rank), str(name),str(platform),int(year), str(genre),str(publisher),float(NA_sales),float(EU_sales),float(JP_sales),float(other_sales),float(global_sales))for rank, name, platform, year, genre, publisher, NA_sales, EU_sales, JP_sales, other_sales, global_sales in lector]
         return res
 def num_juegos_mas_ventas_JP(lista_juegos):
     lista_aux = []
@@ -33,7 +33,20 @@ def juegos_distribuidora_anyo(lista_juegos, publisher, anyo):
 def num_distribuidoras(lista_juegos):
     res = {Juego.publisher for Juego in lista_juegos}
     return len(res)
+
 def juego_mas_antiguo(lista_juegos):
-    pass
+    
+    años ={Juego.year for Juego in lista_juegos}
+    año_mas_antiguo = min(años)
+    res = [Juego for Juego in lista_juegos if año_mas_antiguo == Juego.year]
+    # de esta forma devulve todos los que tengan el año
+    return res
+
+def genero_mas_presente(lista_juegos):
+    lista_generos = [Juego.genre for Juego in lista_juegos]
+    dic_contador= Counter(lista_generos)
+    lista_tupla = [(clave, valor) for clave, valor in dic_contador.items()]
+    lista_tupla.sort(key=lambda x:x[1], reverse=True)
+    return lista_tupla[:1]
     
         
